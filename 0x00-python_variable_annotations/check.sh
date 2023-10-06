9cdd3676a3f0cb5d19d68be8280171da7f4cef9e
Aaron-Ontoyin <aarononto90@gmail.com>
2023-10-06 00:40:58 +0000

    Task 4-define_variables.py

--- 0x00-python_variable_annotations/check.sh
+++ 0x00-python_variable_annotations/check.sh
@@ -1,3 +1,20 @@
-chmod u+x $1
-pycodestyle $1
-mypy $1
+#!/bin/bash
+
+chmod u+x "$1"
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+pycodestyle "$1"
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+mypy "$1"
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+git add .
+git commit -m "Task $1"
+git push
