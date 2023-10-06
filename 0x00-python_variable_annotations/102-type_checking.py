38ddf443e102502f5722f02fd7e807053079190f
Aaron-Ontoyin <aarononto90@gmail.com>
2023-10-06 02:48:42 +0000

    Task 102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -2,20 +2,20 @@
 """
 Type Checking Module. Advanced for function zoom Array
 """
-from typing import List, Generator, Any
+from typing import Sequence, Generator, Any
 
 
-def zoom_array(lst: List[int], factor: float = 2) -> List[Any]:
+def zoom_array(lst: Sequence[int], factor: float = 2) -> Sequence[Any]:
     """Zoom Array function: Zooms an array factor times"""
-    zoomed_in: List[Any] = [
+    zoomed_in: Sequence[Any] = [
         item for item in lst
         for i in range(int(factor))
     ]
     return zoomed_in
 
 
-array: List[int] = [12, 72, 91]
+array: Sequence[int] = [12, 72, 91]
 
-zoom_2x: List[Any] = zoom_array(array)
+zoom_2x: Sequence[Any] = zoom_array(array)
 
-zoom_3x: List[Any] = zoom_array(array, 3.0)
+zoom_3x: Sequence[Any] = zoom_array(array, 3.0)
