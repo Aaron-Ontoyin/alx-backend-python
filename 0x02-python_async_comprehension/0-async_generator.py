fa09ff41b3e893fe69a4afd7c48813feeb83c955
Aaron-Ontoyin <aarononto90@gmail.com>
2023-10-10 14:01:29 +0000

    limit

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator() -> Generator[float, None, None]:
     """ Async Generator: Actually does nothing."""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 11)
+        yield random.uniform(0, 10)
