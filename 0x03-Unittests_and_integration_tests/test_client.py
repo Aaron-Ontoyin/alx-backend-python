7223f89bd1741ee201089f20984ed8270daa4b42
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-10-30 13:50:13 +0000

    task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,16 +2,17 @@
 """Test client Module"""
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
-GithubOrgClient = __import__('client').GithubOrgClient
+
+GithubOrgClient = __import__("client").GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """Github Org Client Test Class"""
 
     @parameterized.expand(["google", "abc"])
-    @patch('client.get_json')
+    @patch("client.get_json")
     def test_org(self, org_name, mock_get_json):
         """Test Org"""
         test_class = GithubOrgClient(org_name)
@@ -19,3 +20,19 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}"
         )
+
+    def test_public_repos_url(self):
+        """Test Public Repos URL property"""
+
+        mock_payload = {
+            "repos_url": "https://api.github.com/orgs/test_org/repos"
+        }
+
+        with patch.object(
+            GithubOrgClient, "org", new_callable=PropertyMock
+        ) as mock_property:
+            mock_property.return_value = mock_payload
+            test_class = GithubOrgClient("test_org")
+            self.assertEqual(
+                test_class._public_repos_url, mock_payload["repos_url"]
+            )
