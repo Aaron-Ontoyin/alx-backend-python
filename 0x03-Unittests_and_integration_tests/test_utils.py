dcbe88f50d2697ac5cdccbe3601eb60bb42eada7
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-10-30 12:18:26 +0000

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,3 +19,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test Access Nested Map"""
         self.assertEqual(utils.access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """Test Access Nested Map Exception"""
+        with self.assertRaises(expected):
+            utils.access_nested_map(nested_map, path)
