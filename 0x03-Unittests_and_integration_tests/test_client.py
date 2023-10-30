e2a25eea456e1ef32a77318cbbaa1321fa976271
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-10-30 13:38:58 +0000

    task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
 """Test client Module"""
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+GithubOrgClient = __import__('client').GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Github Org Client Test Class"""
+
+    @parameterized.expand(["google", "abc"])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """Test Org"""
+        test_class = GithubOrgClient(org_name)
+        test_class.org()
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+        )
